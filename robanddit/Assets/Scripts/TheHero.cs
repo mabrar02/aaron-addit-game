76956296df39dcd99a0caaf391bbb049125720ec
mabrar02 <mahdeen02@gmail.com>
2022-10-01 01:42:04 -0300

    Basic Character Movement
    - WASD/Arrow key movement implemented
    - Spacebar hold jump height mechanic

--- robanddit/Assets/Scripts/TheHero.cs
+++ robanddit/Assets/Scripts/TheHero.cs
@@ -4,35 +4,60 @@ using UnityEngine;
 
 public class TheHero : MonoBehaviour
 {
-   private Rigidbody2D rb; 
-   public float moveSpeed = 5;
+    private float moveSpeed = 8f;
+    private float jumpForce = 20f;
+    private float horizontal;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Rigidbody2D rb;
 
    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        movePlayer();
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+
+        
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
     }
 
-    void movePlayer(){
-       if(Input.GetKey("a")){       
-          transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World);
-       }
-       if (Input.GetKey("s")) {
-          transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
-       }
-       if (Input.GetKey("d")) {
-          transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
-       } 
-       if (Input.GetKey("w")) {
-          transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
-       }    
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
     }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+
+        }
+    }
+
 
 
 }
