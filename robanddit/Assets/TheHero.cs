2e188c09a451b9ceb9e10f6322c54c1d68bbd614
aaron <aaronyu15@hotmail.com>
2022-09-30 20:11:59 -0400

    Changes to be committed: 	new file:   robanddit/Assets/.CameraScript.cs.swp 	new file:   robanddit/Assets/.TheHero.cs.swp 	new file:   robanddit/Assets/CameraScript.cs 	new file:   robanddit/Assets/CameraScript.cs.meta 	new file:   robanddit/Assets/Scenes/StartScene.unity 	new file:   robanddit/Assets/Scenes/StartScene.unity.meta 	new file:   robanddit/Assets/TheHero.cs 	new file:   robanddit/Assets/TheHero.cs.meta - files for the hero

---
+++ robanddit/Assets/TheHero.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheHero : MonoBehaviour
+{
+   private Rigidbody2D rb; 
+
+   // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>();
+      rb.velocity = new Vector2(-10, 0); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
